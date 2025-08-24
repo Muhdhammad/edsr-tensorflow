8e0cb902ae66f0593d57d420c528d7e00e5b90bd
Hammad <muhdhammad04@gmail.com>
2025-08-24 05:31:42 +0500

    Add metrics.py - psnr and ssim

---
+++ src/metrics.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+def calculate_psnr(sr, hr, eps=1e-10):
+    mse = tf.reduce_mean(tf.square(sr - hr), axis=[1,2,3])   # H x W x C
+    return 20 * tf.math.log(1.0 / tf.math.sqrt(mse + eps)) / tf.math.log(10.0)
+
+def calculate_ssim(sr, hr):
+    return tf.image.ssim(sr, hr, max_val=1.0)
