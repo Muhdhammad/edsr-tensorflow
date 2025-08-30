98fede02a2f0948a91ab700b91b3b872328630d7
Hammad <muhdhammad04@gmail.com>
2025-08-30 17:37:41 +0500

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -3,11 +3,11 @@ import numpy as np
 from PIL import Image
 from sklearn.model_selection import train_test_split
 from tensorflow.python.keras.utils import Sequence
-from .augmentations import crop_patches, flip_images
+from .augmentations import crop_patches, flip_images, rotate_images
 
 def create_data_splits(hr_image_dir, val_size=0.2, test_size=0.1, random_state=42, log=True):
 
-    extensions = ['jpg', 'jpeg', 'png', 'bmp']
+    extensions = ['jpg', 'jpeg', 'png', 'bmp', 'tiff']
     image_paths = []
 
     for dir_path, _, files in os.walk(hr_image_dir):
@@ -32,15 +32,15 @@ def create_data_splits(hr_image_dir, val_size=0.2, test_size=0.1, random_state=4
 def create_datasets(train_path, val_path, test_path, hr_size, lr_size, batch_size, log=True):
 
     train_dataset = SRDataset(
-        train_path, batch_size=batch_size, hr_size=hr_size, lr_size=lr_size, do_flip=True, mode='train'
+        train_path, batch_size=batch_size, hr_size=hr_size, lr_size=lr_size, do_flip=True, rotate=True, mode='train'
         )
     
     val_dataset = SRDataset(
-        val_path, batch_size=batch_size, hr_size=hr_size, lr_size=lr_size, do_flip=False, mode='val'
+        val_path, batch_size=batch_size, hr_size=hr_size, lr_size=lr_size, do_flip=False, rotate=False, mode='val'
         )
     
     test_dataset = SRDataset(
-        test_path, batch_size=batch_size, hr_size=hr_size, lr_size=lr_size, do_flip=False, mode='test'
+        test_path, batch_size=batch_size, hr_size=hr_size, lr_size=lr_size, do_flip=False, rotate=False, mode='test'
         )        
 
     if log:
@@ -51,14 +51,19 @@ def create_datasets(train_path, val_path, test_path, hr_size, lr_size, batch_siz
     return train_dataset, val_dataset, test_dataset
 
 class SRDataset(Sequence):
-
-    def __init__(self, image_paths, batch_size, hr_size=256, lr_size=64, val_size=(1200,800), do_flip=False, mode="train"):  
+    """
+    Custom dataset class which gives pair of HR and LR images, it also supports various data augmentations for training
+    like random crop patches, flip images horizontally or vertically, random image rotation and 
+    returns normalized arrays [0,1]
+    """
+    def __init__(self, image_paths, batch_size, hr_size=256, lr_size=64, val_size=(1200,800), do_flip=False, rotate=False, mode="train"):  
         self.hr_images = image_paths
         self.batch_size = batch_size
         self.hr_size = hr_size
         self.lr_size = lr_size
         self.val_size = val_size
         self.flip = do_flip
+        self.rotate = rotate
         self.mode = mode
 
     def __len__(self):
@@ -80,6 +85,8 @@ class SRDataset(Sequence):
                 hr, lr = crop_patches(hr, self.hr_size)
                 if self.flip:
                     hr, lr = flip_images(hr, lr)
+                if self.rotate:
+                    hr, lr = rotate_images(hr, lr)
 
             else:
                 hr = hr.resize(self.val_size)
