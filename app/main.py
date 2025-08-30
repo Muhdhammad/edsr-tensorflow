b77a53eef201a2d4f31b3f3466c1b17eb4031358
Hammad <muhdhammad04@gmail.com>
2025-08-30 20:04:34 +0500

    Added app/main.py for fastapi

---
+++ app/main.py
@@ -0,0 +1,86 @@
+import os
+import io
+import numpy as np
+import tensorflow as tf
+import uvicorn
+from pathlib import Path
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import JSONResponse, StreamingResponse
+from keras.saving import register_keras_serializable
+from PIL import Image
+import logging
+import time
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+app = FastAPI(title="Super Resolution Image")
+
+# Register the custom upscale function used in Lambda layer
+@register_keras_serializable()
+def upscale(x):
+    return tf.nn.depth_to_space(x, block_size=2)  # scale factor = 2
+
+model_path = Path(__file__).parent / "final_sr_model.keras"
+
+try:
+    model = tf.keras.models.load_model(str(model_path), custom_objects={'upscale':upscale}, compile=False)
+    logger.info(f"Model successfully loaded from {model_path}")
+
+except Exception as e:
+    logger.error(f"Failed to load model from {model_path}: {e}")
+
+def preprocess_image(image: UploadFile):
+
+    try:
+        img = Image.open(image.file).convert("RGB")
+        img_arr = np.array(img, dtype=np.float32) / 255.0
+        img_arr = np.expand_dims(img_arr, axis=0)
+
+        return img_arr
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+def super_resolution_image(image):
+
+    try:
+        sr_image = model.predict(image, verbose=0)
+        sr_image = (np.clip(sr_image, 0, 1) * 255).astype(np.uint8)
+        return sr_image[0]
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Model prediction failed {str(e)}")
+
+@app.post("/predict", summary="Super Resolution Image")
+async def predict_image(image: UploadFile = File(...)):
+
+    if not image.content_type.startswith("image/"):
+            raise HTTPException(status_code=500, detail="Unsupported file type")
+
+    try:
+        logger.info(f"Received Image {image.filename}")
+        start_time = time.perf_counter()
+
+        img_array = preprocess_image(image)
+        sr_img = super_resolution_image(img_array)
+
+        end_time = time.perf_counter()
+        infer_time = end_time-start_time
+        logger.info(f"4x Super Resolution completed for {image.filename} in {infer_time:.2f} sec")
+
+        # sr_image = (np.clip(sr_img, 0, 1) * 255).astype(np.uint8)
+        sr_pil = Image.fromarray(sr_img)
+        sr_out = io.BytesIO()
+        sr_pil.save(sr_out, format="PNG")
+        sr_out.seek(0)
+
+        return StreamingResponse(sr_out, media_type="image/png")
+    
+    except Exception as e:
+        logger.exception("Prediction failed")
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/health")
+async def health_check():
+    return JSONResponse(content={"status": "healthy"})
